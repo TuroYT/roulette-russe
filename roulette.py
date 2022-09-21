224eb74217e44fc61f79b03047f5ce0b02b3aeb2
TuroYT <53913510+TuroYT@users.noreply.github.com>
2022-09-21 21:05:42 +0200

    Create roulette.py

---
+++ roulette.py
@@ -0,0 +1,23 @@
+from os import remove
+from random import randint
+from tkinter import *
+from tkinter.messagebox import showerror, showinfo, showwarning
+ 
+def roulette():
+    a = randint(1, 6)
+    b = randint(1, 6)
+    print(a, b)
+    if a==b:
+        showwarning("Aye","Votre PC s'est prix une balle :[")
+ 
+        showerror("Aye","Au revoir...")
+ 
+        remove("temp.txt")
+    else:
+        showinfo("Ouf",f"Tout va bien ! votre balle étais à {abs(b-a)} trous de se faire toucher")
+ 
+ 
+win = Tk("Roulette Russe by Turo YT")
+bouton = Button(win, text="Lancer la Roulette", command=roulette, cursor="pirate")
+bouton.pack(pady=100, padx=100)
+win.mainloop()
